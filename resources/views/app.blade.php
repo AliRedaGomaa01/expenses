ff2a405c65f102e76e27ad27ddd5e2e13487afe6
Ali Reda <alyredagomaa@gmail.com>
2024-11-28 13:18:51 +0200

    edit website icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="{{ asset('assets/logo.svg') }}?v=1">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
