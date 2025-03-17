2d7f6c95b9d22393a9fc49bca49c20acdcf246d9
Ali Reda <alyredagomaa@gmail.com>
2025-03-17 13:13:32 +0200

    update summary order

--- resources/js/Pages/Expenses/Components/ExpenseTable.jsx
+++ resources/js/Pages/Expenses/Components/ExpenseTable.jsx
@@ -4,41 +4,42 @@ import groupBy from '@/Helpers/groupBy';
 import EditCurrentExpenses from './EditCurrentExpenses';
 
 export default function ExpenseTable({ expenses, expenseData, filters, categories, ...props }) {
-    expenses?.data?.forEach((expense) => {
-        expense['date'] = expense['date']['date'] ?? expense['date'];
-    });
+  expenses?.data?.forEach((expense) => {
+    expense['date'] = expense['date']['date'] ?? expense['date'];
+  });
 
-    const groupedExpenses = groupBy('date', [...expenses?.data]);
+  const groupedExpenses = groupBy('date', [...expenses?.data]);
 
-    const pagination = { ...expenses };
-    delete pagination.data;
+  const pagination = { ...expenses };
+  delete pagination.data;
 
-    return (
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 my-4">
-            <div className="overflow-hidden my-grad shadow-sm sm:rounded-lg">
-                <div className="p-6 text-gray-900 text-center">
+  return (
+    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 my-4">
+      <div className="overflow-hidden my-grad shadow-sm sm:rounded-lg">
+        <div className="p-6 text-gray-900 text-center">
 
-                    {!expenses?.data.length && <div className='text-center p-2 shadow-gray-400 shadow-md my-4'>'لا يوجد مصاريف'</div>}
+          {!expenses?.data.length && <div className='text-center p-2 shadow-gray-400 shadow-md my-4'>'لا يوجد مصاريف'</div>}
 
-                    {!!expenses?.data.length && <div className="overflow-scroll scrollbar-hide">
+          {!!expenses?.data.length && <div className="overflow-scroll scrollbar-hide space-y-5">
 
-                        {Object.keys(groupedExpenses).map((date , index) => (
-                            <div key={index}>
-                                <div className='text-center p-2  bg-gradient-to-r from-red-200 to-sky-200  my-4 font-extrabold '>
-                                    {date} 
-                                </div>
+            <ExpenseSummary expenseData={expenseData} filters={filters} />
 
-                                <EditCurrentExpenses expenses={ [...groupedExpenses[date]] } categories={ categories }/>
-                            </div>
-
-                        ))}
-
-                        <Pagination {...pagination} />
-
-                        <ExpenseSummary expenseData={expenseData} filters={filters} />
-                    </div>}
+            {Object.keys(groupedExpenses).map((date, index) => (
+              <div key={index}>
+                <div className='text-center p-2  bg-gradient-to-r from-red-200 to-sky-200  my-4 font-extrabold '>
+                  {date}
                 </div>
-            </div>
+
+                <EditCurrentExpenses expenses={[...groupedExpenses[date]]} categories={categories} />
+              </div>
+
+            ))}
+
+            <Pagination {...pagination} />
+
+          </div>}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
